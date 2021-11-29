7813173b5423f3736fc7158c4320073986a77a76
udaygupta <udaygupta9594@gmail.com>
2021-11-29 07:55:46 +0530

    day2

---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class homepage extends StatelessWidget {
+  final int days = 10;
+  final String name = "uday";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First app'),
+      ),
+      body: Material(
+          child: Center(
+        child: Container(
+          child: Text('${days} daya of flutter '),
+        ),
+      )),
+    );
+  }
+}
